864ea21f25a41ba42fad56260c8e2379ce602c68
Djamalutin <fisher.gerryy@mail.ru>
2023-08-05 14:04:34 +0300

    feat: creating Redux Reducer and Store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import UserList from "./components/UserList";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
 }
 
 export default App;
